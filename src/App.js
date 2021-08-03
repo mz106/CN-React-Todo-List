ffc36352c75beb42118ff0aacfded7beb45bc568
dave franks <flashman123@fastmail.fm>
2021-08-03 12:46:20 +0100

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React from 'react';
+
+const TodoItem = ( { task, handleDelete, index }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>{task}</h2>
+      <button onClick={() => handleDelete(index)}>delete</button>
     </div>
-  );
+  )
+}
+
+class App extends React.Component {
+
+  state = {
+    todos: [],
+    userInput: ''
+  }
+
+  handleDelete = (index) => {
+    console.log('clicked')
+    let newArr = [...this.state.todos];
+    newArr.splice(index, 1);
+    this.setState({ todos : newArr })
+  }
+
+  handleChange = (event) => {
+    this.setState({ userInput: event.target.value })
+  }
+
+  handleAdd = (event) => {
+    event.preventDefault();
+    let newArr = [...this.state.todos];
+    newArr.push(this.state.userInput);
+    this.setState( { todos: newArr})
+    this.setState({ userInput: ''})
+    console.log(this.state.todos)
+  }
+    
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>Michael's todo list!</h1>
+          <form onSubmit={this.handleAdd}>
+            <input onChange={this.handleChange} value={this.state.userInput}/>
+            <button type="submit">add</button>
+          </form>
+        </div>
+        <div>
+          {this.state.todos.map((task, index) => {
+            return <TodoItem task={task} key={index} index={index} handleDelete={this.handleDelete}/>
+          })}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
